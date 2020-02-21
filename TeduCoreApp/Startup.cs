9c95d5c856955cec5948eb6af4cea6f4611e9eea
huy nguyen duc <huynd.aptech@hotmail.com>
2020-02-21 11:08:35 +0700

    a

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -29,7 +29,7 @@ namespace TeduCoreApp
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>() 
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
